5accd8b46e79238a558e44ef3b1d13259b727821
Kristina Asenova <krisinceto_98@abv.bg>
2020-02-12 00:38:59 +0200

    Added ExportToExcel

--- SalaryCalculator/Controllers/ExportController.cs
+++ SalaryCalculator/Controllers/ExportController.cs
@@ -3,60 +3,78 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using SalaryCalculator.Data;
 using SalaryCalculator.Services.Contracts;
+using SalaryCalculator.Web.Models;
+using System.Web;
+using Microsoft.Azure.Amqp.Framing;
 
 namespace SalaryCalculator.Web.Controllers
 {
     public class ExportController : Controller
     {
         private readonly IBulgariaSalaryService salaryService;
+        private readonly SalaryCalculatorDbContext context;
 
-        public ExportController(IBulgariaSalaryService salaryService)
+
+        public ExportController(IBulgariaSalaryService salaryService, SalaryCalculatorDbContext context)
         {
             this.salaryService = salaryService;
+            this.context = context;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        //[HttpGet]
-        //public async Task<IActionResult> CreateDocument()
+        //public IActionResult Index()
         //{
-        //	//Create an instance of ExcelEngine
-        //	using (ExcelEngine excelEngine = new ExcelEngine())
-        //	{
-        //		IApplication application = excelEngine.Excel;
+        //    var salaryList = this.context
+        //        .Salaries
+        //        .Select(x => new SalaryViewModel
+        //        {
+        //            PersonEmail = x.PersonEmail,
+        //            GrossSalary = x.GrossSalary,
+        //            NetSalary = x.NetSalary
+        //        }).ToList();
 
-        //		application.DefaultVersion = ExcelVersion.Excel2016;
+        //    return View(salaryList);
+        //}
 
-        //		//Create a workbook
-        //		IWorkbook workbook = application.Workbooks.Create(1);
-        //		IWorksheet worksheet = workbook.Worksheets[0];
+        //public void ExportToExcel()
+        //{
+        //    var salaryList = this.context
+        //        .Salaries
+        //        .Select(x => new SalaryViewModel
+        //        {
+        //            PersonEmail = x.PersonEmail,
+        //            GrossSalary = x.GrossSalary,
+        //            NetSalary = x.NetSalary
+        //        }).ToList();
 
-        //		//Adding a picture
-        //		//FileStream imageStream = new FileStream("AdventureCycles-Logo.png", FileMode.Open, FileAccess.Read);
-        //		//IPictureShape shape = worksheet.Pictures.AddPicture(1, 1, imageStream);
+        //    ExcelPackage pack = new ExcelPackage();
 
-        //		//Disable gridlines in the worksheet
-        //		worksheet.IsGridLinesVisible = false;
+        //    ExcelWorksheet ws = pack.Workbook.Worksheets.Add("Report");
+            
+        //    ws.Cells["A1"].Value = "Email";
+        //    ws.Cells["B1"].Value = "GrossSalary";
+        //    ws.Cells["C1"].Value = "NetSalary";
 
-        //		//Saving the Excel to the MemoryStream 
-        //		MemoryStream stream = new MemoryStream();
+        //    int rowStart = 2;
+        //    foreach(var salary in salaryList)
+        //    {
+        //        ws.Row(rowStart).Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+        //        ws.Cells[string.Format("A{0}", rowStart)].Value = salary.PersonEmail;
+        //        ws.Cells[string.Format("B{0}", rowStart)].Value = salary.GrossSalary;
+        //        ws.Cells[string.Format("C{0}", rowStart)].Value = salary.NetSalary;
+        //        rowStart++;
+        //    }
 
-        //		workbook.SaveAs(stream);
-
-        //		//Set the position as '0'.
-        //		stream.Position = 0;
-
-        //		//Download the Excel file in the browser
-        //		FileStreamResult fileStreamResult = new FileStreamResult(stream, "application/excel");
-
-        //		fileStreamResult.FileDownloadName = "Output.xlsx";
-
-        //		return fileStreamResult;
-        //	}
+        //    ws.Cells["A:AZ"].AutoFitColumns();
+        //    Response.Clear();
+        //    Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+        //    Response.AppendTrailer("content-disposition", "attachment: filename=" + "ExcelReport.xlsx");
+        //    //Response.BinaryWrite(pack.GetAsByteArray());
+        //    //Response.End();
+        //}
     }
 }
--- SalaryCalculator/Controllers/HomeController.cs
+++ SalaryCalculator/Controllers/HomeController.cs
@@ -3,16 +3,78 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using SalaryCalculator.Data;
 using SalaryCalculator.Models;
+using SalaryCalculator.Services.Contracts;
+using SalaryCalculator.Web.Models;
 
 namespace SalaryCalculator.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly IBulgariaSalaryService salaryService;
+        private readonly SalaryCalculatorDbContext context;
+
+
+        public HomeController(IBulgariaSalaryService salaryService, SalaryCalculatorDbContext context)
+        {
+            this.salaryService = salaryService;
+            this.context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var salaryList = this.context
+                .Salaries
+                .Select(x => new SalaryViewModel
+                {
+                    PersonEmail = x.PersonEmail,
+                    GrossSalary = x.GrossSalary,
+                    NetSalary = x.NetSalary
+                }).ToList();
+
+            return View(salaryList);
+        }
+
+        public void ExportToExcel()
+        {
+            var salaryList = this.context
+                .Salaries
+                .Select(x => new SalaryViewModel
+                {
+                    PersonEmail = x.PersonEmail,
+                    GrossSalary = x.GrossSalary,
+                    NetSalary = x.NetSalary
+                }).ToList();
+
+            ExcelPackage pack = new ExcelPackage();
+
+            ExcelWorksheet ws = pack.Workbook.Worksheets.Add("Report");
+
+            ws.Cells["A1"].Value = "Email";
+            ws.Cells["B1"].Value = "GrossSalary";
+            ws.Cells["C1"].Value = "NetSalary";
+
+            int rowStart = 2;
+            foreach (var salary in salaryList)
+            {
+                ws.Row(rowStart).Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+                ws.Cells[string.Format("A{0}", rowStart)].Value = salary.PersonEmail;
+                ws.Cells[string.Format("B{0}", rowStart)].Value = salary.GrossSalary;
+                ws.Cells[string.Format("C{0}", rowStart)].Value = salary.NetSalary;
+                rowStart++;
+            }
+
+            Response.Clear();
+            Response.ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+            //Response.AddHeader("content-disposition", "attachment: filename=" + "ExcelReport.xlsx");
+            //Response.BinaryWrite(pack.GetAsByteArray());
+            //Response.End();
+
+
         }
 
         public IActionResult Privacy()
